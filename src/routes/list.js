96c059b620f98fb25270d86411a4d15dffceb751
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 16:11:03 +0300

    feat: add get list items API

--- src/routes/list.js
+++ src/routes/list.js
@@ -4,3 +4,12 @@ const db = require("../db");
 const router = new Router();
 module.exports = router;
 
+// Get list items
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await db.query(
+    "SELECT list_name, name, rate, publication_date FROM user_to_see_list WHERE list_id = $1",
+    [id]
+  );
+  res.status(200).json(rows);
+});
