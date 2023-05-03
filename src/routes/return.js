f7e73be3d1e3d1a5d8a2c1b81d244b86c800418a
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-04 00:42:06 +0300

    feat: Add finish rental process API

--- src/routes/return.js
+++ src/routes/return.js
@@ -11,3 +11,22 @@ router.get("/", async (req, res) => {
   );
   res.status(200).json(rows);
 });
+
+// Finish the rental process and add the record to rental history
+router.post("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  try {
+    await db.query("CALL add_return_history ($1, $2::VARCHAR)", [
+      id,
+      description,
+    ]);
+    res.status(200).json("The return history was successfully added.");
+  } catch {
+    (err) => {
+      console.error(err);
+      res.status(500).json("An error occured while adding rental history.");
+    };
+  }
+  res.status(409).json("Record does not exist.");
+});
