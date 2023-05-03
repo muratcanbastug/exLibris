f8d8032630883db4ff5e6fef3f7cacd5680ee963
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 16:10:28 +0300

    Add list router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const users = require("./user");
+const lists = require("./list");
 
 module.exports = (app) => {
   app.use("/users", users);
+  app.use("/lists", lists);
 };
---
+++ src/routes/list.js
@@ -0,0 +1,6 @@
+const Router = require("express-promise-router");
+const bcrypt = require("bcrypt");
+const db = require("../db");
+const router = new Router();
+module.exports = router;
+
