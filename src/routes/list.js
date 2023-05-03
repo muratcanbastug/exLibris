f3ecdb055fb264130770f004d6ad5ce36352cc21
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 18:31:53 +0300

    feat: Add add item to the list API

--- src/routes/list.js
+++ src/routes/list.js
@@ -49,3 +49,31 @@ router.patch("/:id", async (req, res) => {
     };
   }
 });
+
+// Add item to list
+router.post("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { item_id } = req.body;
+  try {
+    const { rows } = await db.query(
+      "SELECT COUNT(*) FROM item_list WHERE list_id = $1 AND item_id = $2",
+      [id, item_id]
+    );
+    if (rows[0].count > 0) {
+      res.status(409).json("The item is already in the list.");
+    } else {
+      await db.query(
+        "INSERT INTO item_list (list_id, item_id) VALUES ($1, $2)",
+        [id, item_id]
+      );
+      res.status(200).json("The item was added to the list successfully.");
+    }
+  } catch {
+    (err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "An error occurred while adding the item to the list.",
+      });
+    };
+  }
+});
