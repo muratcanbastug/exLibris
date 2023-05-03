d237b2990ddb15bc9a912503b8beeff025403887
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 23:34:50 +0300

    feat: Add update due date API

--- src/routes/rental.js
+++ src/routes/rental.js
@@ -33,3 +33,28 @@ router.post("/:id", async (req, res) => {
     .status(409)
     .json({ message: "Item is not available or user has max item." });
 });
+
+// Update due date
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { user_id } = req.body;
+
+  try {
+    const { rows } = await db.query(
+      "CALL update_due_date_on_rental($1::INTEGER, $2::INTEGER, $3::Date)",
+      [id, user_id, "01-01-2000"]
+    );
+    res.status(200).json({ due_date: rows[0].p_due_date });
+  } catch {
+    (err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "An error occurred while updating due date" });
+    };
+  }
+  res.status(409).json({
+    message:
+      "Maximum extra time request has been reached or record does not exist.",
+  });
+});
