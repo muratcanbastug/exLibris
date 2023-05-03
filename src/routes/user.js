5b5f6a120075bc02df311294fe77e7e9464b09ac
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 15:59:38 +0300

    feat: add user lists API

--- src/routes/user.js
+++ src/routes/user.js
@@ -146,3 +146,12 @@ router.post("/:id/ban", async (req, res) => {
       .json({ error: "An error occurred while banning the user." });
   }
 });
+
+// Get user's all lists
+router.get("/:id/lists", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await db.query("SELECT * FROM list WHERE user_id = $1", [
+    id,
+  ]);
+  res.status(200).json(rows);
+});
