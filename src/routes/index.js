0d5dcd06c18ff3cd72018b045af592d6c43e23ac
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-03 20:42:53 +0300

    fix: Update ban user file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const users = require("./user");
 const lists = require("./list");
+const bans = require("./ban");
 
 module.exports = (app) => {
   app.use("/users", users);
   app.use("/lists", lists);
+  app.use("/bans", bans);
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -118,37 +118,6 @@ router.patch("/:id/reset-password", async (req, res) => {
   }
 });
 
-// Ban an user
-router.post("/:id/ban", async (req, res) => {
-  const { id } = req.params;
-  const { admin_id, report } = req.body;
-  try {
-    // Check if user with the given id_number is already banned
-    const { rows } = await db.query(
-      "SELECT banned FROM user_account WHERE user_id = $1",
-      [id]
-    );
-
-    if (rows[0].banned) {
-      // User is already banned
-      res.status(409).json({
-        error: "User is already banned.",
-      });
-    } else {
-      await db.query(
-        "CALL add_banned_user($1::INTEGER, $2::INTEGER, $3::VARCHAR)",
-        [id, admin_id, report]
-      );
-      res.status(200).json({ user_id: id });
-    }
-  } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ error: "An error occurred while banning the user." });
-  }
-});
-
 // Get user's all lists
 router.get("/:id/lists", async (req, res) => {
   const { id } = req.params;
