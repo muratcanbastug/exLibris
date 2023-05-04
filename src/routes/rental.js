33d43c9997a424b79239aec3617c1953dcdafea7
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-04 23:13:34 +0300

    feat: Add get the item rental log API

--- src/routes/rental.js
+++ src/routes/rental.js
@@ -9,6 +9,16 @@ router.get("/", async (req, res) => {
   res.status(200).json(rows);
 });
 
+// Get the item rental at that time
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await db.query(
+    "SELECT * FROM all_current_rentals WHERE item_id = $1",
+    [id]
+  );
+  res.status(200).json(rows);
+});
+
 // Add rental
 router.post("/:id", async (req, res) => {
   const { id } = req.params;
