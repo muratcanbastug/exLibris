b0c70795b81ebd091830055d83e835e9910a01fb
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-04 20:05:23 +0300

    feat: Add delete branch API

--- src/routes/branch.js
+++ src/routes/branch.js
@@ -9,3 +9,20 @@ router.get("/", async (req, res) => {
   const { rows } = await db.query("SELECT * FROM branch_shelf");
   res.status(200).json(rows);
 });
+
+// Delete branch
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db.query("CALL delete_branch($1::INTEGER)", [id]);
+    res.status(200).json("The delete was successful.");
+  } catch {
+    (err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while deleting the branch." });
+    };
+  }
+  res.status(409).json("There are items in the branch.");
+});
