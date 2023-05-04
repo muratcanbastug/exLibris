61a4728851b1189d7ab4f28e96a9e163a00fd028
Murat Can Bastug <51405534+muratcanbastug@users.noreply.github.com>
2023-05-04 20:20:28 +0300

    feat: Add add new admin API

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,7 @@
 const Router = require("express-promise-router");
 const db = require("../db");
 const router = new Router();
+const bcrypt = require("bcrypt");
 
 module.exports = router;
 
@@ -8,4 +9,45 @@ module.exports = router;
 router.get("/", async (req, res) => {
   const { rows } = await db.query("SELECT * FROM admin");
   res.status(200).json(rows);
+});
+
+// Add new admin
+router.post("/", async (req, res) => {
+  const { first_name, last_name, email, username, password, phone_number } =
+    req.body;
+  try {
+    // Check if admin with the given email already exists
+    const { rows: existingRows } = await db.query(
+      "SELECT email FROM admin WHERE email = $1::VARCHAR",
+      [email]
+    );
+
+    if (existingRows.length > 0) {
+      // Admin already exists, return their admin name
+      res.status(409).json({
+        admin_id: existingRows[0].admin_id,
+        error: "Admin already exists.",
+      });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const { rows } = await db.query(
+        "CALL add_admin($1::VARCHAR, $2::VARCHAR, $3::VARCHAR, $4::VARCHAR, $5::VARCHAR, $6::VARCHAR, $7::INTEGER)",
+        [
+          first_name,
+          last_name,
+          email,
+          username,
+          hashedPassword,
+          phone_number,
+          1,
+        ]
+      );
+      res.status(200).json({ admin_id: rows[0].p_admin_id });
+    }
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while adding the admin." });
+  }
 });
