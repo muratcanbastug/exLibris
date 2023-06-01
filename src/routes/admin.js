91dc4cfbd0c7ed479352157be0887ab57d9c16a0
Murat Can Bastug <muratcanbastug1@gmail.com>
2023-06-01 20:52:41 +0300

    feat: Add Admin Auth to Admin Routers

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,17 +2,21 @@ const Router = require("express-promise-router");
 const db = require("../db");
 const router = new Router();
 const bcrypt = require("bcrypt");
+const {
+  adminAuthMiddleware,
+  adminAndLoggedAuthMiddleware,
+} = require("../security/authMiddlware");
 
 module.exports = router;
 
-// Get all branches with their shelves
-router.get("/", async (req, res) => {
+// Get all admins information
+router.get("/", adminAuthMiddleware, async (req, res) => {
   const { rows } = await db.query("SELECT * FROM admin");
   res.status(200).json(rows);
 });
 
 // Add new admin
-router.post("/", async (req, res) => {
+router.post("/", adminAuthMiddleware, async (req, res) => {
   const { first_name, last_name, email, username, password, phone_number } =
     req.body;
   try {
@@ -53,13 +57,13 @@ router.post("/", async (req, res) => {
 });
 
 // Update admin information
-router.patch("/:id", async (req, res) => {
-  const { id } = req.params;
+router.patch("/", adminAndLoggedAuthMiddleware, async (req, res) => {
+  const { admin_id } = req.tokenPayload;
   const { first_name, last_name, email, username, phone_number } = req.body;
   try {
     await db.query(
       "CALL update_admin_information($1::INTEGER, $2::VARCHAR, $3::VARCHAR, $4::VARCHAR, $5::VARCHAR, $6::VARCHAR)",
-      [id, first_name, last_name, email, username, phone_number]
+      [admin_id, first_name, last_name, email, username, phone_number]
     );
     res.status(200).json({ result: "Admin information updated successfully." });
   } catch (err) {
@@ -71,20 +75,24 @@ router.patch("/:id", async (req, res) => {
 });
 
 // Reset admin password
-router.patch("/:id/reset-password", async (req, res) => {
-  const { id } = req.params;
-  const { new_password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(new_password, 10);
-    await db.query(
-      "UPDATE admin SET password = $1::VARCHAR WHERE admin_id = $2::INTEGER",
-      [hashedPassword, id]
-    );
-    res.status(200).json({ result: "Admin password updated successfully." });
-  } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ error: "An error occurred while updating the password." });
+router.patch(
+  "/reset-password",
+  adminAndLoggedAuthMiddleware,
+  async (req, res) => {
+    const { admin_id } = req.tokenPayload;
+    const { new_password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(new_password, 10);
+      await db.query(
+        "UPDATE admin SET password = $1::VARCHAR WHERE admin_id = $2::INTEGER",
+        [hashedPassword, admin_id]
+      );
+      res.status(200).json({ result: "Admin password updated successfully." });
+    } catch (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while updating the password." });
+    }
   }
-});
+);
