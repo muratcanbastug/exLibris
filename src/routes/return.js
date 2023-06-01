5cca28e134119b3f780f4ba6b9de9ea74e5fa9f2
Murat Can Bastug <muratcanbastug1@gmail.com>
2023-06-01 23:48:54 +0300

    feat: Add Auth to Return Routers

--- src/routes/return.js
+++ src/routes/return.js
@@ -1,19 +1,36 @@
 const Router = require("express-promise-router");
 const db = require("../db");
 const router = new Router();
+const {
+  authMiddleware,
+  adminAuthMiddleware,
+} = require("../security/authMiddlware");
 
 module.exports = router;
 
 // Get all return records
-router.get("/", async (req, res) => {
+router.get("/", adminAuthMiddleware, async (req, res) => {
   const { rows } = await db.query(
     "SELECT * FROM return_history ORDER BY return_date"
   );
   res.status(200).json(rows);
 });
 
+// Get user return records
+router.get("/user", authMiddleware, async (req, res) => {
+  const { user_id } = req.tokenPayload;
+  if (user_id === undefined) {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
+  const { rows } = await db.query(
+    "SELECT * FROM return_history WHERE user_id = $1 ORDER BY return_date",
+    [user_id]
+  );
+  res.status(200).json(rows);
+});
+
 // Finish the rental process and add the record to rental history
-router.post("/:id", async (req, res) => {
+router.post("/:id", adminAuthMiddleware, async (req, res) => {
   const { id } = req.params;
   const { description } = req.body;
   try {
