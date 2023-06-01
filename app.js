0fad2b4a4aa7f0aaf79915d19cc33dcf744aec58
Murat Can Bastug <muratcanbastug1@gmail.com>
2023-06-01 20:02:52 +0300

    fix: cors

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mountRoutes = require("./src/routes");
 const dotenv = require("dotenv");
 dotenv.config();
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 const bodyParser = express.json;
 app.use(bodyParser());
 
